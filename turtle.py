79325fd85bdfb41f9991561c8ce5b2524cf79d1e
Dong Kuen <danaver12@daum.net>
2022-07-07 15:20:42 +0900

    main_modi

--- turtle.py
+++ turtle.py
@@ -2,54 +2,21 @@ import turtle as t
 import random
 import time
 
-def game_play(playing):
-    current_time = time.time()
-    maximum = current_time + 30
-
-    while playing:
-        if time.time() > maximum:
-            playing = False
-            return playing
-
-        player.showturtle()
-        player.up()
-        location = random.choice(location_list)
-        player.goto(location)
-        sleep_time = random.uniform(1, 2)
-        time.sleep(sleep_time)
-
-score = 0
-
-def turn_right():
-    global score
-    if player.position() == (200.00, 0.00):
-        player.hideturtle()
-        score = score + 1
-        show_score()
-
-
-def show_message(up, down):
-    player.goto(0,200)
-    player.write(up, False, "center", (20, "bold"))
-    player.goto(0,-200)
-    player.write(down, False, "center", (15, "bold"))
-    player.goto(0,0)
-    player.showturtle()
-    
-
 def screen_setting():
     screen = t.Screen()
     screen.title("Catch Turtle") # 그래픽 창 이름 지정
     screen.setup(500, 500) # 창 크기 500*500으로 설정
-    return screen
 
 def score_board_setting():
     score_board = t.Turtle()
+    color = input("Color of score_board : ")
     score_board.color("white") # 보드판 색깔 지정
     score_board.goto(150,150)
-    return score_board
 
-def main:
+def main():
+
+    shape = input("shape : ") # refer to turtle help
+
     player = t.Turtle() # 거북이 객체 생성
     player.shape("turtle")
     player.speed(0)
@@ -58,17 +25,19 @@ def main:
     screen = screen_setting() # screen 객체 생성
     score_board = score_board_setting() # score_board 객체 생성
 
-   
 
     location_list = [(0,200), (0,-200), (200,0), (-200,0)] # 거북이의 위치(위, 아래, 오른쪽, 왼쪽) 리스트로 생성
 
-    screen.onkeypress(start, "space") # 스페이스 바를 누르면 start 함수 실행
-    screen.onkeypress(right, "Right") # 오른쪽 키를 누르면 right 함수 실행
-    screen.onkeypress(left, "Left")
-    screen.onkeypress(up, "Up")
-    screen.onkeypress(down, "Down")
+    screen.onkeypress(game_start, "space") # 스페이스 바를 누르면 start 함수 실행
+    screen.onkeypress(turn_right, "Right") # 오른쪽 키를 누르면 right 함수 실행
+    screen.onkeypress(turn_left, "Left")
+    screen.onkeypress(turn_up, "Up")
+    screen.onkeypress(turn_down, "Down")
     screen.listen() # 이 명령어를 실행시켜야 키 입력모드가 실행되어 입력된 키에 반응
 
-    message("Let's Catch Turtle!", "[Space]") # 게임 시작하기 전 첫 화면으로 "Let's Catch Turtle!"과 "[Space]"를 출력
+    message("Let's Catch Turtle!", "[Space]") # 게임 시작하기 전 첫 화면으로 "Catch Turtle"과 "[Space]"를 출력
 
-    input()
+print("Welcome to the Catch Turtle Game!")
+initial_score = int(input("initial_score : "))
+score = initial_score # 획득 점수
+main() # 메인 함수 호출
