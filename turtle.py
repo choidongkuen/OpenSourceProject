a8844c2c33e2e7e208b5ecb7d7b88a32954c4d62
Dong Kuen <danaver12@daum.net>
2022-07-08 14:22:03 +0900

    second modifi 7/8

--- turtle.py
+++ turtle.py
@@ -103,8 +103,14 @@ player.shape(shape)
 player.speed(0)
 player.up()
 
-screen = screen_setting() # screen 객체 생성
-score_board = score_board_setting() # score_board 객체 생성
+screen = t.Screen()
+screen.title("Catch Turtle") # 그래픽 창 이름 지정
+screen.setup(500, 500) # 창 크기 500*500으로 설정
+
+score_board = t.Turtle()
+color = input("Color of score_board : ")
+score_board.color(color) # 보드판 색깔 지정
+score_board.goto(150,150)
 
 
 location_list = [(0,200), (0,-200), (200,0), (-200,0)] # 거북이의 위치(위, 아래, 오른쪽, 왼쪽) 리스트로 생성
