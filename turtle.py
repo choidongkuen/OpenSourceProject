03af40d7a1b8b6d290cdcc9fa88a74157edbde01
Dong Kuen <danaver12@daum.net>
2022-07-08 14:29:29 +0900

    last commit

--- turtle.py
+++ turtle.py
@@ -6,9 +6,9 @@ score = 0
 
 def show_message(up, down): # 메세지 출력
     player.goto(0,200)
-    player.write(up, False, "center", (20, "bold"))
+    player.write(up, False, "center", ("Arial", 20, "bold"))
     player.goto(0,-200)
-    player.write(down, False, "center", (15, "bold"))
+    player.write(down, False, "center", ("Arial",15, "normal"))
     player.goto(0,0)
     player.showturtle()
 
@@ -16,7 +16,7 @@ def show_message(up, down): # 메세지 출력
 def start(): # 게임 시작
     playing = True
     player.clear()
-    playing = play(playing)
+    playing = game_play(playing)
     game_end(playing)
 
 
@@ -31,7 +31,7 @@ def game_play(playing): # 게임 진행 함수
 
         player.showturtle()
         player.up()
-        location = random.choice(locatoin_list)
+        location = random.choice(location_list)
         player.goto(location)
         sleep_time = random.uniform(1, 2)
         time.sleep(sleep_time)
@@ -41,8 +41,8 @@ def game_end(playing): # 게임 종료 함수
     global score
     if not playing:
         score_board.clear()
-        message("Game Over!", text)
         text = "Your Score : %d" % score
+        show_message("Game Over!", text)
         score = 0
 
 def turn_up(): # 오른쪽 방향키 함수
@@ -73,22 +73,30 @@ def turn_right(): # 왼쪽 방향키 함수
         player.hideturtle()
         score = score + 1
         show_score()
-'''
-def screen_setting(): # screen 객체 설정 함수
 
-    screen = t.Screen()
-    screen.title("Catch Turtle") # 그래픽 창 이름 지정
-    screen.setup(500, 500) # 창 크기 500*500으로 설정
-    return screen
+def show_score(score): # 점수 출력
+    score_board.clear()
+    score_board.color("white")
+    score_board.goto(150, 150)
+    score_board.pencolor("black")
+    score_board.write("Score : %d" % score, False, "left", ("Arial", 13, "bold"))
+    score_board.color("white")
 
-def score_board_setting(): # score_board 객체 설정 함수
+#def screen_setting(): # screen 객체 설정 함수
 
-    score_board = t.Turtle()
-    color = input("Color of score_board : ")
-    score_board.color("white") # 보드판 색깔 지정
-    score_board.goto(150,150)
-    return score_board
-'''
+ #   screen = t.Screen()
+ #   screen.title("Catch Turtle") # 그래픽 창 이름 지정
+ #   screen.setup(500, 500) # 창 크기 500*500으로 설정
+ #   return screen
+
+#def score_board_setting(): # score_board 객체 설정 함수
+
+ #   score_board = t.Turtle()
+  #  color = input("Color of score_board : ")
+  #  score_board.color("white") # 보드판 색깔 지정
+  #  score_board.goto(150,150)
+  #  return score_board
+
 # def main(): # 메인 함수
 
 
@@ -111,11 +119,9 @@ score_board = t.Turtle()
 color = input("Color of score_board : ")
 score_board.color(color) # 보드판 색깔 지정
 score_board.goto(150,150)
-
-
 location_list = [(0,200), (0,-200), (200,0), (-200,0)] # 거북이의 위치(위, 아래, 오른쪽, 왼쪽) 리스트로 생성
 
-screen.onkeypress(game_start, "space") # 스페이스 바를 누르면 start 함수 실행
+screen.onkeypress(start, "space") # 스페이스 바를 누르면 start 함수 실행
 screen.onkeypress(turn_right, "Right") # 오른쪽 키를 누르면 right 함수 실행
 screen.onkeypress(turn_left, "Left")
 screen.onkeypress(turn_up, "Up")
@@ -123,3 +129,6 @@ screen.onkeypress(turn_down, "Down")
 screen.listen() # 이 명령어를 실행시켜야 키 입력모드가 실행되어 입력된 키에 반응
 
 show_message("Let's Catch Turtle!", "[Space]") # 게임 시작하기 전 첫 화면으로
+
+t.done()
+
